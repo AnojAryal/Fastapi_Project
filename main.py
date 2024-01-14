530b2d65f473a3654461b32e4203af9372738d9b
anojaryal <anoj1810@gmail.com>
2024-01-14 13:35:37 +0545

    Enumeration  path parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 from typing import Optional
 from pydantic import BaseModel
 
@@ -11,6 +12,12 @@ BOOKS = {
     'book_2': {'title':'Title_2','author': 'Author_2'},
     'book_3': {'title':'Title_3','author': 'Author_3'},
 }
+
+class DirectionName(str , Enum):
+    north = 'North'
+    south = 'South'
+    east = 'East'
+    west = 'West'
 
 @app.get("/")
 async def books():
@@ -25,3 +32,13 @@ async def read_book(book_title):
 @app.get("/book_id/{book_id}")
 async def read_book(book_id: int):
     return {'Book Id': book_id}
+
+@app.get("/directions/{direction_name}")
+async def get_direction(direction_name: DirectionName):
+    if direction_name == DirectionName.north:
+        return {'Direction': direction_name, 'sub':'Up'}
+    if direction_name == DirectionName.south:
+        return {'Direction': direction_name, 'sub':'Down'}
+    if direction_name == DirectionName.east:
+        return {'Direction': direction_name, 'sub':'Left'}
+    return {'Direction': direction_name, 'sub':'Right'}
