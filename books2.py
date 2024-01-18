69f14bbf1db89f6765a65d0344085688b479a50c
anojaryal <anoj1810@gmail.com>
2024-01-18 13:24:10 +0545

    basemodel configurations

--- books2.py
+++ books2.py
@@ -6,18 +6,29 @@ from uuid import UUID
 
 app = FastAPI()
 
-
 class Book(BaseModel):
     id: UUID
     title: str = Field(min_length=1)
-    author: str = Field(min_length = 1, max_length = 100)
+    author: str = Field(min_length=1, max_length=100)
     description: Optional[str] = Field(
         title="Description of the book",
         max_length=100,
         min_length=1,
         default=None
     )
-    rating: int = Field(gt =-1 , lt= 101)
+    rating: int = Field(gt=-1, lt=101)
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'id': '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+                'title': 'Computer Science Pro',
+                'author': 'FraNzY',
+                'description': 'A very nice Description of book',
+                'rating': 75
+            }
+        }
+
 
 BOOKS  = []
 
@@ -36,22 +47,22 @@ async def create_book(book : Book):
 
 
 def create_book_no_api():
-    book_1 = BOOKS(id ='90e571fb-6083-4668-9aba-919d59aa878f', 
+    book_1 = Book(id ='90e571fb-6083-4668-9aba-919d59aa878f', 
                 title = 'Title 1', 
                 author = 'Author 1', 
                 description = 'Description 1', 
                 rating = 60 )
-    book_2 = BOOKS(id ='80e571fb-6083-4668-9aba-919d59aa878f', 
+    book_2 = Book(id ='80e571fb-6083-4668-9aba-919d59aa878f', 
                 title = 'Title 2', 
                 author = 'Author 2', 
                 description = 'Description 2', 
                 rating = 70 )
-    book_3 = BOOKS(id ='70e571fb-6083-4668-9aba-919d59aa878f', 
+    book_3 = Book(id ='70e571fb-6083-4668-9aba-919d59aa878f', 
                 title = 'Title 3', 
                 author = 'Author 3', 
                 description = 'Description 3', 
                 rating = 80 )
-    book_4 = BOOKS(id ='60e571fb-6083-4668-9aba-919d59aa878f', 
+    book_4 = Book(id ='60e571fb-6083-4668-9aba-919d59aa878f', 
                 title = 'Title 4', 
                 author = 'Author 4', 
                 description = 'Description 4', 
