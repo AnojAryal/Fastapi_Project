535a1a19b893f12e96b374a2f3c646ac66e65172
anojaryal <anoj1810@gmail.com>
2024-01-24 20:07:16 +0545

    get all todos from database

--- Project/Todo/database.py
+++ Project/Todo/database.py
@@ -5,10 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args = {'check_same_thread' : False}
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
 )
 
-SessionLocal = sessionmaker(autocommit = False, autoFlush = False, Bind = engine)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- Project/Todo/main.py
+++ Project/Todo/main.py
@@ -1,14 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 import models
-from database import engine
-
+from database import engine, SessionLocal
 
 app = FastAPI()
 
+# Create database tables on startup
+models.Base.metadata.create_all(bind=engine)
 
-models.Base.metadata.create_all(bind = engine)
-
+# Dependency to get a database session
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 @app.get('/')
-async def create_database():
-    return {'Database' : 'Created'}
+async def read_all(db: Session = Depends(get_db)):
+    # Query all Todos from the database
+    todos = db.query(models.Todos).all()
+    return todos
