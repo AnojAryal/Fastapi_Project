ecf4bd8adf037873d4f396e7ff2ff08fbceba967
anojaryal <anoj1810@gmail.com>
2024-01-26 20:25:55 +0545

    delete request

--- Project/Todo/main.py
+++ Project/Todo/main.py
@@ -55,11 +55,7 @@ async def create_todo(todo:Todo, db:Session= Depends(get_db)):
 
     db.add(todo_model)
     db.commit()
-
-    return {
-        'status': 201,
-        'transaction': 'Success'
-    }
+    return successful_response(201)
 
 
 @app.put('/{todo_id}')
@@ -77,15 +73,32 @@ async def update_todo(todo_id : int, todo: Todo, db:Session= Depends(get_db)):
 
     db.add(todo_model)
     db.commit()
+    return successful_response(200)
 
+
+@app.delete('/{todo_id}')
+async def delete_todo(todo_id : int, db:Session= Depends(get_db)):
+    todo_model = db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .first()
+
+    if todo_model is None:
+        raise http_exception()
+
+    todo_model = db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .delete()
+
+    db.commit()
+    return successful_response(200)
+
+
+def successful_response(status_code : int):
     return {
-        'status': 200,
+        'status': status_code,
         'transaction': 'Success'
     }
 
 
-
 def http_exception():
     return HTTPException(status_code=404, detail=f'Todo not found')
-
-
