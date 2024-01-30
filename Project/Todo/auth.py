5a7a8d096d8b014b8f9785c18df8d403ab4bb891
anojaryal <anoj1810@gmail.com>
2024-01-30 18:56:50 +0545

    get current user token

--- Project/Todo/auth.py
+++ Project/Todo/auth.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
@@ -78,6 +78,19 @@ def create_access_token(username:str, user_id: int,
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
+#get current user token
+async def get_current_user(token : str = Depends(oauth2_bearer)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id : int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=404, detail='user not found!')
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=404, detail='user not found!')
+
+
 
 
 @app.post('/create/user')
@@ -98,7 +111,6 @@ async def create_new_user(create_user: CreateUser, db:Session = Depends(get_db))
     db.commit()
 
     return 'successful'
-
 
 
 @app.post('/token')
