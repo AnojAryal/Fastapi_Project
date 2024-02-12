6bdeaf57a604c4f93521b11ad1ac2f136ad88c40
anojaryal <anoj1810@gmail.com>
2024-02-12 21:22:45 +0545

    get user by path parameter

--- Project/Todo/routers/users.py
+++ Project/Todo/routers/users.py
@@ -31,3 +31,11 @@ def get_db():
 @router.get('/')
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Users).all()
+
+
+@router.get('/user/{user_id}')
+async def user_by_path(user_id: int,db: Session = Depends(get_db)):
+    user_model = db.query(models.Users).filter(models.Users.id ==user_id).first()
+    if user_model is not None:
+        return user_model
+    return 'Invalid user id'
