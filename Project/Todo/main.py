fc010647bff4bdb2eb14d4cb95bf07df85647214
anojaryal <anoj1810@gmail.com>
2024-02-12 19:38:48 +0545

    read all users

--- Project/Todo/main.py
+++ Project/Todo/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import models
 from database import engine
-from routers import auth, todos
+from routers import auth, todos , users
 from company import company_apis
 
 app = FastAPI()
@@ -11,4 +11,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(users.router)
 app.include_router(company_apis.router)
--- Project/Todo/routers/users.py
+++ Project/Todo/routers/users.py
@@ -7,4 +7,27 @@ import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
-from .auth import get_current_user, get_user_exception, verify_password
+from .auth import get_current_user, get_user_exception, verify_password, get_password_hash
+
+
+router = APIRouter(
+    prefix= '/users',
+    tags=['users'],
+    responses={404: {'Description': 'Not found'}}
+)
+
+
+models.Base.metadata.create_all(bind = engine)
+
+# Dependency to get a database sessions
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@router.get('/')
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Users).all()
