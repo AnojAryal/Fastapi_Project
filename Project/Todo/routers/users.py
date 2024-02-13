1542dd8f14f2e6259a5c6a4da2d429fc4ca6f2f6
anojaryal <anoj1810@gmail.com>
2024-02-13 20:11:39 +0545

    modify the user password

--- Project/Todo/routers/users.py
+++ Project/Todo/routers/users.py
@@ -28,10 +28,18 @@ def get_db():
         db.close()
 
 
+class UserVerification(BaseModel):
+    username : str
+    password : str
+    new_password : str
+
+
+
+
 @router.get('/')
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Users).all()
-
+ 
 
 @router.get('/user/{user_id}')
 async def user_by_path(user_id: int,db: Session = Depends(get_db)):
@@ -47,3 +55,23 @@ async def user_by_query(user_id: int,db: Session = Depends(get_db)):
     if user_model is not None:
         return user_model
     return 'Invalid user id'
+
+
+@router.put('/user/password')
+async def user_password_change(user_verification :UserVerification, user : dict = Depends(get_current_user),
+                            db : Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    user_model = db.query(models.Users).filter(models.Users.id == user.get('id')).first()
+
+    if user_model is not None:
+        if user_verification.username == user_model.username and verify_password(
+                user_verification.password, 
+                user_model.hashed_password):
+
+            user_model.hashed_password = get_password_hash(user_verification.new_password)
+            db.add(user_model)
+            db.commit()
+            return 'Successful'
+    return 'Invalid user or request!'
+
