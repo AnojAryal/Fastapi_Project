d163440fd9e5f675ef03cae0eb505230b957b069
anojaryal <anoj1810@gmail.com>
2024-03-04 20:59:44 +0545

    refac : logout error fixed

--- Project/Todo/routers/auth.py
+++ Project/Todo/routers/auth.py
@@ -117,11 +117,10 @@ async def get_current_user(request: Request):
         username: str = payload.get('sub')
         user_id : int = payload.get('id')
         if username is None or user_id is None:
-            return None
+            logout(request)
         return {'username': username, 'id': user_id}
     except JWTError:
         raise get_user_exception()
-
 
 
 @router.post('/create/user')
