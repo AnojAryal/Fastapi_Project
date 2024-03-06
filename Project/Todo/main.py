42ad8161f3d56cc43ef6f9086452d8f03ee72288
anojaryal <anoj1810@gmail.com>
2024-03-06 19:47:19 +0545

    code clean up

--- Project/Todo/main.py
+++ Project/Todo/main.py
@@ -3,7 +3,8 @@ import models
 from database import engine
 from routers import auth, todos, users, address
 from company import company_apis
-
+from starlette import status
+from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -12,6 +13,10 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
+
+@app.get('/')
+async def root():
+    return RedirectResponse(url='todos',status_code=status.HTTP_302_FOUND)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
