243c152dce83d9d7e3fc06e0864333aa7b4a7a5f
anojaryal <anoj1810@gmail.com>
2024-03-09 15:25:50 +0545

    change password function completed

--- Project/Todo/routers/users.py
+++ Project/Todo/routers/users.py
@@ -52,6 +52,28 @@ async def edit_user_view(request : Request):
 
 
 
+router.post('/edit-password',response_class= HTMLResponse )
+async def user_password_change(request: Request, username: str = Form(...),
+                                password: str = Form(...), password2: str = Form(...),
+                                db: Session = Depends(get_db)):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url='/auth', status_code=status.HTTP_302_FOUND)
+
+    user_data = db.query(models.Users).filter(models.Users.username == username).first()
+
+    msg = 'Invalid Username or Password!!'
+
+    if user_data is not None:
+        if username == user_data.username and verify_password(password, user_data.hashed_password):
+            user_data.hashed_password = get_password_hash(password2)
+            db.add(user_data)
+            db.commit()
+            msg = 'Password Updated Successfully!'
+
+    return templates.TemplateResponse('edit-user-password.html', {'request': request, 'user': user, 'msg': msg})
+
+
 @router.get('/')
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Users).all()
